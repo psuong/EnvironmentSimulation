8bcc3f07ac3da6b55c781035a1ee3b14085373af
Porrith Suong <psuong95@gmail.com>
2015-04-07 02:04:38 -0400

    Random plant generation per cycle and initial writing to file.

--- src/Ecosystem.java
+++ src/Ecosystem.java
@@ -1,10 +1,12 @@
+import java.io.*;
+import java.nio.file.Files;
 import java.util.Random;
 
 /**
  * Created by porrith on 4/6/15.
  */
 public class Ecosystem {
-    final private int dimension = 32;
+    final private int dimension = 10;
     protected Cell field[][] = new Cell[dimension][dimension];
 
     public Ecosystem()
@@ -24,6 +26,19 @@ public class Ecosystem {
             }
             System.out.println("\n");
         }
+        System.out.println("\n");
+    }
+
+    public void writeFile() throws IOException{
+        PrintWriter write = new PrintWriter("out.txt", "UTF-8");
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                String line = "[" + field[i][j].objects.getSymbol() + "|" + field[i][j].animals.getSymbol() + "] ";
+                write.append(line);
+            }
+            write.append("\n");
+        }
+        write.close();
     }
 
     public boolean isAvail(int x, int y, char c)
@@ -41,11 +56,11 @@ public class Ecosystem {
 
     public void initSpawn()
     {
-        int amount = new Random().nextInt(1024);
+        int amount = new Random().nextInt(50);
         for (int i = 0; i < amount; i++) {
             int random = new Random().nextInt(5);
-            int x = new Random().nextInt(32);
-            int y = new Random().nextInt(32);
+            int x = new Random().nextInt(dimension);
+            int y = new Random().nextInt(dimension);
             if ((random == 0) && (isAvail(x, y, '#'))) {
                 field[x][y].objects = new Obstacle(x, y);
             }
@@ -63,9 +78,26 @@ public class Ecosystem {
         }
     }
 
-    public static void main(String[] args) {
+    public void plantGen(int x, int y)
+    {
+        if (isAvail(x, y, '*'))
+        {
+            System.out.println(true);
+            field[x][y].objects = new Plant(x, y);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         Ecosystem environ = new Ecosystem();
         environ.initSpawn();
         environ.printEco();
+        for (int i = 0; i < 10; i++) {
+            int x = new Random().nextInt(10);
+            int y = new Random().nextInt(10);
+            environ.plantGen(x, y);
+            environ.printEco();
+            environ.writeFile();
+        }
+
     }
 }
